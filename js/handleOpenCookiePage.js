399b4a7a8558bd72dafe5462fe00288e1a820075
vanja-veapi <vanja.veapi@gmail.com>
2026-03-15 22:31:57 +0100

    Refactor cookie page handling to improve readability and maintainability

--- js/handleOpenCookiePage.js
+++ js/handleOpenCookiePage.js
@@ -8,48 +8,51 @@ const HOURS_PER_DAY = 24;
 const ONE_DAY_IN_MS =
 	HOURS_PER_DAY * MINUTES_PER_HOUR * SECONDS_PER_MINUTE * MS_PER_SECOND; // 86_400_000
 
+const redirectHome = (message) => {
+	alert(message);
+	window.location.replace('/');
+};
+
 export const handleOpenCookiePage = () => {
 	if (!window.location.hash) {
-		alert('Treba da budes redirektovan');
-		window.location.replace('/');
+		redirectHome('Treba da budes redirektovan');
 		return;
 	}
 
 	const now = Date.now();
-	const { timestamp: dateTheMessageWasSent } = getHashParams();
+	const { sentAt_timestamp } = getHashParams();
 
-	if (now - dateTheMessageWasSent > ONE_DAY_IN_MS) {
-		alert('Isteklo je vreme za pregled poruke');
-		window.location.replace('/');
+	if (now - sentAt_timestamp > ONE_DAY_IN_MS) {
+		redirectHome('Isteklo je vreme za pregled poruke');
 		return;
 	}
 
 	const btnOpenCookie = document.querySelector('#btn-open-cookie');
 
-	btnOpenCookie.addEventListener('click', renderMessageFromHash);
+	btnOpenCookie.addEventListener('click', renderMessageFromHash, {
+		once: true,
+	});
 };
 
 const renderMessageFromHash = () => {
-	const { timestamp, message, signature } = getHashParams();
+	const { sentAt_timestamp, message, signature } = getHashParams();
 
-	renderMessagePaper({ message, signature, date: new Date(timestamp) });
+	renderMessagePaper({ message, signature, date: new Date(sentAt_timestamp) });
 };
 
 const renderMessagePaper = ({ message, signature, date }) => {
-	const containerId = 'message-container';
-	if (document.querySelector(`#${containerId}`)) return;
-
 	const container = createDOMElement('main', {
 		style: {
 			fontSize: '24px',
 			marginTop: '20px',
 			border: '2px solid red',
 		},
-		id: containerId,
 	});
 
 	container.appendChild(createDOMElement('p', { content: message }));
-	container.appendChild(createDOMElement('p', { content: `Datum: ${date}` }));
+	container.appendChild(
+		createDOMElement('p', { content: `Datum: ${date.toLocaleString()}` }),
+	);
 
 	if (signature) {
 		container.appendChild(
@@ -61,11 +64,10 @@ const renderMessagePaper = ({ message, signature, date }) => {
 };
 
 const createDOMElement = (tagName, options = {}) => {
-	const { content, style, id } = options;
+	const { content, style } = options;
 
 	const element = document.createElement(tagName);
 
-	if (id) element.id = id;
 	if (content) element.textContent = content;
 
 	if (style) {
@@ -82,6 +84,6 @@ const getHashParams = () => {
 	return {
 		message: decoded.get('message'),
 		signature: decoded.get('signature'),
-		timestamp: Number(decoded.get('date')),
+		sentAt_timestamp: Number(decoded.get('date')),
 	};
 };
