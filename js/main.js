77d55c1b1016df2f0ce0ef947b686775816546ad
vanja-veapi <vanja.veapi@gmail.com>
2026-03-17 15:23:56 +0100

    feat: refactor routing logic to improve path handling for GitHub Pages

--- js/main.js
+++ js/main.js
@@ -6,9 +6,18 @@ const routes = {
 	'/open-cookie.html': openCookiePage,
 };
 
+// Since github pages return <url>/un-fortune-cookie/ instead of <url>/ we need to check if the pathname includes 3rd element
+// /un-fortune-cookie/open-cookie.html
+// ! localhost -> /open-cookie.html
+function getRoutePath() {
+	const pathname = window.location.pathname;
+	const file = pathname.split('/').pop();
+
+	return file ? `/${file}` : '/index.html';
+}
+
 function runRoute() {
-	const path =
-		window.location.pathname === '/' ? '/index.html' : window.location.pathname;
+	const path = getRoutePath();
 
 	routes[path]();
 }
